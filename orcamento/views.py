4859a0c08b7ef9e9f18d2e795ea94e1be0ef03a3
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2022-01-28 21:19:08 -0300

    Add serializers and viewsets

--- orcamento/views.py
+++ orcamento/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from orcamento.models import Receita, Despesa
+from orcamento.serializer import ReceitaSerializer, DespesaSerializer
 
-# Create your views here.
+
+class ReceitasViewSet(viewsets.ModelViewSet):
+    """Exibindo todas as receitas"""
+
+    queryset = Receita.objects.all()
+    serializer_class = ReceitaSerializer
+
+
+class DespesasViewSet(viewsets.ModelViewSet):
+    """Exibindo todas as despesas"""
+
+    queryset = Despesa.objects.all()
+    serializer_class = DespesaSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from orcamento.views import ReceitasViewSet, DespesasViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('receitas', ReceitasViewSet, basename='Receitas')
+router.register('despesas', DespesasViewSet, basename='Despesas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
